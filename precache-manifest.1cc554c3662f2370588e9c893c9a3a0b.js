258c2ae81a5400f298147f6256dea766bb1626bb
Elias Rodelo <eliasrodeloso@gmail.com>
2020-10-11 20:38:07 -0500

    Updates

---
+++ precache-manifest.1cc554c3662f2370588e9c893c9a3a0b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ef85b04f04dab18991da6eed9cd394",
+    "url": "/stardust/index.html"
+  },
+  {
+    "revision": "2d54c78d5c534b677a7d",
+    "url": "/stardust/static/css/main.d24d291b.chunk.css"
+  },
+  {
+    "revision": "0c9c6a20c2a516b01909",
+    "url": "/stardust/static/js/2.d42acaf6.chunk.js"
+  },
+  {
+    "revision": "f55be4b6139ec68af09f3b7204918315",
+    "url": "/stardust/static/js/2.d42acaf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d54c78d5c534b677a7d",
+    "url": "/stardust/static/js/main.6a824f56.chunk.js"
+  },
+  {
+    "revision": "ea8c29a5da59e63c159f",
+    "url": "/stardust/static/js/runtime-main.f963a350.js"
+  },
+  {
+    "revision": "2bf08001f42c34e30e1de9d2eb81aead",
+    "url": "/stardust/static/media/FiraCode-Bold.2bf08001.woff"
+  },
+  {
+    "revision": "8236190c08c5efe01d04baae252c0241",
+    "url": "/stardust/static/media/FiraCode-Bold.8236190c.ttf"
+  },
+  {
+    "revision": "9ecee2ce95b0de731b1498b48e71c737",
+    "url": "/stardust/static/media/FiraCode-Bold.9ecee2ce.woff2"
+  },
+  {
+    "revision": "f0f92eeb8c1af44fa4e604069fde97d9",
+    "url": "/stardust/static/media/FiraCode-Bold.f0f92eeb.eot"
+  },
+  {
+    "revision": "259c44f16f7596bab6718b235119e013",
+    "url": "/stardust/static/media/FiraCode-Light.259c44f1.eot"
+  },
+  {
+    "revision": "3a7fa12881064e3e0e3983d8524bbc00",
+    "url": "/stardust/static/media/FiraCode-Light.3a7fa128.woff2"
+  },
+  {
+    "revision": "a85ebfc3913ab6bc6a7b817fa627b904",
+    "url": "/stardust/static/media/FiraCode-Light.a85ebfc3.ttf"
+  },
+  {
+    "revision": "a8a711df192aa3da76989bcb5db3239d",
+    "url": "/stardust/static/media/FiraCode-Light.a8a711df.woff"
+  },
+  {
+    "revision": "010c2e55a9ef14a61d1d6c8b3fbcc7bc",
+    "url": "/stardust/static/media/FiraCode-Medium.010c2e55.ttf"
+  },
+  {
+    "revision": "4dee11c0d1acf3da2be1957cb5299315",
+    "url": "/stardust/static/media/FiraCode-Medium.4dee11c0.woff"
+  },
+  {
+    "revision": "52f4340d65ddb99093e8c3af242c33fc",
+    "url": "/stardust/static/media/FiraCode-Medium.52f4340d.eot"
+  },
+  {
+    "revision": "d293dc7c066d907f308b9924a60f1700",
+    "url": "/stardust/static/media/FiraCode-Medium.d293dc7c.woff2"
+  },
+  {
+    "revision": "24310bd06e2537b92a92f33dabdc038b",
+    "url": "/stardust/static/media/FiraCode-Regular.24310bd0.woff2"
+  },
+  {
+    "revision": "62da0ac51db1e4a3a4d57b9154a344fc",
+    "url": "/stardust/static/media/FiraCode-Regular.62da0ac5.woff"
+  },
+  {
+    "revision": "6929f8ebd787ddbe39bf7be574731a5e",
+    "url": "/stardust/static/media/FiraCode-Regular.6929f8eb.eot"
+  },
+  {
+    "revision": "f08fdb39cbf433548d343b4e4daf3ba1",
+    "url": "/stardust/static/media/FiraCode-Regular.f08fdb39.ttf"
+  },
+  {
+    "revision": "542b7217e4f45935d2be7d76bf9ba08d",
+    "url": "/stardust/static/media/planets.542b7217.svg"
+  }
+]);
